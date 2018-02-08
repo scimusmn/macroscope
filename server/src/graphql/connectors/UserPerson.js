52405e0dcbb52711bed47072f91b3d3211f918e7
bryan kennedy <bkennedy@smm.org>
2018-02-08 10:16:49 -0500

    Removing boilerplate database connectors
    These are from another project.

--- server/src/graphql/connectors/UserPerson.js
+++
@@ -1,5 +0,0 @@
-import sequelize from './sequelize';
-
-const UserPerson = sequelize.define('userPerson');
-
-export default UserPerson;
--- server/src/graphql/connectors/UserRole.js
+++
@@ -1,5 +0,0 @@
-import sequelize from './sequelize';
-
-const UserRole = sequelize.define('userRole');
-
-export default UserRole;
