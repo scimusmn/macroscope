17477ff0c25462c1c447164eb275a2fe22edd020
bryan kennedy <bkennedy@smm.org>
2018-02-08 10:15:59 -0500

    Updating DB env settings
    Removing DB type from the env name
    Adding a setting for the database dialect.

--- server/src/graphql/connectors/sequelize.js
+++ server/src/graphql/connectors/sequelize.js
@@ -5,12 +5,12 @@ import Sequelize from 'sequelize';
 
 // Create MySQL connection
 const sequelize = new Sequelize(
-  process.env.MYSQL_DATABASE,
-  process.env.MYSQL_USER,
-  process.env.MYSQL_PASS,
+  process.env.DB_DATABASE,
+  process.env.DB_USER,
+  process.env.DB_PASS,
   {
-    host: process.env.MYSQL_HOST,
-    dialect: 'mysql',
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_TYPE,
     operatorsAliases: false,
   },
 );
